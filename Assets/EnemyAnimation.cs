cd40f8e7e7da04373479c921bed838e9436c1cc2
cheoljin0324 <77821929+cheoljin0324@users.noreply.github.com>
2025-09-03 17:29:28 +0900

    ㅇㅇ

--- Assets/EnemyAnimation.cs
+++ Assets/EnemyAnimation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class EnemyAnimation : MonoBehaviour
 {
-    //Èûµé´Ù11234
+    //Èûµé´Ù1123411
     public enum State { Idle, Move }
     //Å×½ºÆ®
     public List<Sprite> idleSprites;
